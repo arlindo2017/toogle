a4f12e702945c2a7892b90080484fab70b573c02
abmetheny <abmetheny@gmail.com>
2023-05-31 17:30:04 -0400

    Made minor changes to the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,11 +20,12 @@ const Navbar = () => {
                         <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
                     </a>
                     <ul className="p-2">
-                        <li><a>Electrical</a></li>
-                        <li><a>Landscaping</a></li>
+                        <li><Link to="/service">Electrical</Link></li>
+                        <li><Link to="/service">Landscaping</Link></li>
+                        <li><Link to="/category">Browse All Services</Link></li>
                     </ul>
                     </li>
-                    <li><a>Item 3</a></li>
+                    {/* <li><a>Item 3</a></li> */}
                 </ul>
                 </div>
                 <Link to="/" className="btn btn-ghost normal-case text-xl">toogle</Link>
@@ -39,12 +40,12 @@ const Navbar = () => {
                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                     </a>
                     <ul className="p-2">
-                    <li><Link to="/service">Electrical</Link></li>
-                    <li><Link to="/service">Landscaping</Link></li>
-                    <li><Link to="/category">Browse All Services</Link></li>
+                        <li><Link to="/service">Electrical</Link></li>
+                        <li><Link to="/service">Landscaping</Link></li>
+                        <li className="font-bold"><Link to="/category">Browse All Services</Link></li>
                     </ul>
                 </li>
-                <li><a>Item 3</a></li>
+                {/* <li><a>Item 3</a></li> */}
                 </ul>
             </div>
 
