9d3b0ffe3e40e97be9582f72435110f655054b42
abmetheny <abmetheny@gmail.com>
2023-06-01 14:22:00 -0400

    Added banners, category cards, and reviews to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,12 +9,172 @@ const Home = () => {
                 <div class="hero-overlay bg-opacity-60"></div>
                 <div class="hero-content text-center text-neutral-content pr-[45vw]">
                     <div class="max-w-md">
-                    <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 class="mb-5 text-5xl font-bold">Welcome to toogle.</h1>
+                    <p>We help you get things done.</p>
+                    <p class="mb-5">By letting you let someone else do it.</p>
                     <button class="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
+
+            {/* category grid */}
+            <div class="flex flex-col w-full">
+                <div class="grid h-10 card mt-20 rounded-box place-items-left text-2xl font-bold pl-10">Popular services</div> 
+            </div>
+
+            <div className="grid-container px-40 py-10">
+                <div class="grid grid-cols-4 gap-4">
+                    {/* card 1 */}
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure><img src={require('../images/moving.jpeg')} alt="furniture assembly" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Furniture Assembly</h2>
+                            <p>Avg Price: $57-128</p>
+                            <div class="card-actions justify-end">
+                                <div class="badge badge-outline">Moving</div> 
+                            </div>
+                        </div>
+                    </div>
+                    {/* card 2 */}
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure><img src={require('../images/moving.jpeg')} alt="furniture assembly" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Furniture Assembly</h2>
+                            <p>Avg Price: $57-128</p>
+                            <div class="card-actions justify-end">
+                                <div class="badge badge-outline">Moving</div> 
+                            </div>
+                        </div>
+                    </div>
+                    {/* card 3 */}
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure><img src={require('../images/moving.jpeg')} alt="furniture assembly" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Furniture Assembly</h2>
+                            <p>Avg Price: $57-128</p>
+                            <div class="card-actions justify-end">
+                                <div class="badge badge-outline">Moving</div> 
+                            </div>
+                        </div>
+                    </div>
+                    {/* card 4 */}
+                    <div class="card bg-base-100 shadow-xl">
+                        <figure><img src={require('../images/moving.jpeg')} alt="furniture assembly" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">Furniture Assembly</h2>
+                            <p>Avg Price: $57-128</p>
+                            <div class="card-actions justify-end">
+                                <div class="badge badge-outline">Moving</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* second banner/hero */}
+            <div class="card lg:card-side bg-base-100 shadow-xl mt-20">
+                <figure className="w-1/2"><img src={require('../images/painting.jpg')} alt="painting"/></figure>
+                <div class="card-body my-auto">
+                    <h2 class="card-title">Turn your to-do list into a to-done list.</h2>
+                    <p className="flex-grow-0">Find service providers for every inch of your house, all in one place.</p>
+                        <li>Search by what's important to you.</li>
+                        <li>View availability and schedule an appointment.</li>
+                        <li>Leave reviews and view your previous orders.</li>
+                </div>
+            </div>
+
+            {/* reviews section */}
+            <div class="flex flex-col w-full">
+                <div class="grid h-10 card mt-20 rounded-box place-items-left text-2xl font-bold pl-10">See what others are saying...</div> 
+            </div>
+            
+            <div class="carousel w-full">
+                <div id="slide1" class="carousel-item relative w-full">
+                    <div class="card w-full bg-base-100 shadow-xl my-5 mx-10 px-5">
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Margaret</h2>
+                            <div className="rating">
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                            </div>
+                            <p>This is the best service ever!!  So easy.</p>
+                            <p>05/23/2023</p>
+                            <div className="badge badge-accent">Plumbing</div>
+                        </div>
+                    </div>
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" class="btn btn-circle">❮</a> 
+                    <a href="#slide2" class="btn btn-circle">❯</a>
+                    </div>
+                </div> 
+                <div id="slide2" class="carousel-item relative w-full">
+                <div class="card w-full bg-base-100 shadow-xl my-5 mx-10 px-5">
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Dave</h2>
+                            <div className="rating">
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            </div>
+                            <p>Great Experience.  Will use again.</p>
+                            <p>05/23/2023</p>
+                            <div className="badge badge-accent">Electrical</div>
+                        </div>
+                    </div>
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" class="btn btn-circle">❮</a> 
+                    <a href="#slide3" class="btn btn-circle">❯</a>
+                    </div>
+                </div> 
+                <div id="slide3" class="carousel-item relative w-full">
+                <div class="card w-full bg-base-100 shadow-xl my-5 mx-10 px-5">
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Sam</h2>
+                            <div className="rating">
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            </div>
+                            <p>I didn't have to lift a finger!</p>
+                            <p>05/23/2023</p>
+                            <div className="badge badge-accent">Plumbing</div>
+                        </div>
+                    </div>
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" class="btn btn-circle">❮</a> 
+                    <a href="#slide4" class="btn btn-circle">❯</a>
+                    </div>
+                </div> 
+                <div id="slide4" class="carousel-item relative w-full">
+                <div class="card w-full bg-base-100 shadow-xl my-5 mx-10 px-5">
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Jerry</h2>
+                            <div className="rating">
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                            </div>
+                            <p>My lawn has never looked better.</p>
+                            <p>05/23/2023</p>
+                            <div className="badge badge-accent">Landscaping</div>
+                        </div>
+                    </div>
+                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" class="btn btn-circle">❮</a> 
+                    <a href="#slide1" class="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 };
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,75 +9,42 @@ const Navbar = () => {
             {/* nav bar for the collapsed hamburger menu */}
             <div className="navbar-start">
                 <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link to="/">Home</Link></li>
-
-                    <li>
-                        <div className="dropdown dropdown-bottom">
-                        <label tabIndex={0}>
-                            <a>
-                                Services
-                                <svg className="fill-current inline-block" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                            </a>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to="/">Home</Link></li>
+                        <li>
+                        <summary>Services</summary>
+                        <ul className="p-2">
+                            <li><Link to="/service">Plumbing</Link></li>
                             <li><Link to="/service">Electrical</Link></li>
                             <li><Link to="/service">Landscaping</Link></li>
-                            <li className="font-bold"><Link to="/category">Browse All Services</Link></li>
+                            <li><Link to="/category">Browse All Categories</Link></li>
                         </ul>
-                        </div>
-                    </li>
-                    
-                    {/* <li tabIndex={0}>
-                    <a className="justify-between">
-                        Services
-                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                    </a>
-                    <ul className="p-2">
-                        <li><Link to="/service">Electrical</Link></li>
-                        <li><Link to="/service">Landscaping</Link></li>
-                        <li><Link to="/category">Browse All Services</Link></li>
+                        </li>
+                        {/* <li><a>Item 3</a></li> */}
                     </ul>
-                    </li> */}
-                    {/* <li><a>Item 3</a></li> */}
-                </ul>
                 </div>
                 <Link to="/" className="btn btn-ghost normal-case text-xl">toogle</Link>
             </div>
-            {/* navbar for the full page desktop site */}
+            
+            {/* nave bar for full page desktop */}
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link to="/">Home</Link></li>
-                    {/* <li tabIndex={0}>
-                        <a>
-                        Services
-                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><Link to="/service">Electrical</Link></li>
-                            <li><Link to="/service">Landscaping</Link></li>
-                            <li className="font-bold"><Link to="/category">Browse All Services</Link></li>
-                        </ul>
-                    </li> */}
-                    <li>
-                        <div className="dropdown dropdown-bottom">
-                        <label tabIndex={0}>
-                            <a>
-                                Services
-                                <svg className="fill-current inline-block" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                            </a>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to="/service">Electrical</Link></li>
-                            <li><Link to="/service">Landscaping</Link></li>
-                            <li className="font-bold"><Link to="/category">Browse All Services</Link></li>
-                        </ul>
-                        </div>
-                    </li>
-                    {/* <li><a>Item 3</a></li> */}
+                <li><Link to="/">Home</Link></li>
+                <li tabIndex={0}>
+                    <details>
+                    <summary>Services</summary>
+                    <ul className="p-2">
+                        <li><Link to="/service">Plumbing</Link></li>
+                        <li><Link to="/service">Electrical</Link></li>
+                        <li><Link to="/service">Landscaping</Link></li>
+                        <li><Link to="/category">Browse All Categories</Link></li>
+                    </ul>
+                    </details>
+                </li>
+                {/* <li><a>Item 3</a></li> */}
                 </ul>
             </div>
 
