0f6e679f1958964140a4d2c3a7293906ec0a0749
abmetheny <abmetheny@gmail.com>
2023-06-03 15:30:55 -0400

    Formatted category page and set up categories to import images from db

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -9,20 +9,45 @@ const Category = () => {
     const categories = data?.getAllCategoriesWithServices;
   
     return (
-        <div>
-          {categories &&
-            categories.map((category) => (
-              <div key={category._id} className="card mb-3">
-                <h4 className="card-header bg-primary text-light p-2 m-0">
-                  {category.categoryName} <br />
-                </h4>
-                <div>
-                    <Services 
-                    services={category.services}
-                    />
-                </div>                
-              </div>
-            ))}
+        <div className="page-container">
+            {/* banner */}
+            <div className="hero">
+                <img
+                src={require("../images/categories-banner.jpeg")}
+                alt="man using power drill"
+                className="hero object-cover"
+                ></img>
+                {/* <div className="hero-overlay "></div> */}
+                <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-40 text-5xl font-bold">Help us help you.</h1>
+                </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col w-full border-opacity-50 mt-10 mb-5">
+                <div className="divider text-2xl font-bold">WHAT WE OFFER</div>
+            </div>
+
+            {/* categories */}
+            <div className="category-container flex flex-wrap w-full justify-center px-20">
+                {categories && categories.map((category) => (
+                <div key={category._id} className="card w-72 bg-base-100 shadow-xl m-4">
+                        <figure><img src={require(`../images/${category.categoryImage}`)} alt="Shoes" /></figure>
+                        <div className="card items-center text-center">
+                            <h2 className="card-title mt-5">{category.categoryName}</h2>
+                        </div>
+                        <div className="flex flex-col w-full border-opacity-50">
+                            <div className="divider"></div>
+                        </div>
+                        <div>
+                            <Services 
+                            services={category.services}
+                            />
+                        </div>                
+                </div>
+                ))}
+            </div> 
         </div>
     ); 
 };
