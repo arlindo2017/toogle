f3bb9c1119337da6fed4e42b8382ed56548d584e
abmetheny <abmetheny@gmail.com>
2023-06-04 15:33:22 -0400

    get_me query functioning and populates user account page

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Orders from "./Orders";
 
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+
 const User = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const userData = data?.me || [];
+
+
   return (
     <div>
       <div className="card mx-56 bg-base-100 shadow-xl mt-10 -z-50">
@@ -10,20 +17,21 @@ const User = () => {
           <div className="col-start-2 col-span-2">
             <div className="avatar">
               <div className="w-24 rounded-full">
-                <img src={require('../images/profile-placeholder.png')}  alt="profile placeholder" />
+                <img src={require('../images/profile/profile-placeholder.png')}  alt="profile placeholder" />
               </div>
             </div>
-            <p>Username</p>
-            <p>change username</p>
+            <p>{userData.username}</p>
+            <p>(option to update username)</p>
           </div>
 
           {/* right side card */}
           <div className="col-start-4 col-span-4">
             <h2 className="card-title">Account Information</h2>
             {/* info form */}
-
-            
-            
+            <p>First Name: {userData.firstName}</p>
+            <p>Last Name: {userData.lastName}</p>
+            <p>Email: {userData.email}</p>
+            <p>Password: (option to update password)</p>           
 
           </div>
         </div>
