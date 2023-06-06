ab5eeaf39801a3fe6b972cf2293cae0f8011dbaf
Arlindo Pinto <perensen@gmail.com>
2023-06-06 17:06:18 -0400

    added mutations to client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,33 +1,63 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 //tested 6/2 - working
 export const ADD_USER = gql`
-  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
-  addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
-    user {
-      email
-      firstName
-      isProvider
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      user {
+        email
+        firstName
+        isProvider
+      }
+      token
     }
-    token
   }
-}
 `;
 
 //tested 6/2 - working
 export const LOGIN = gql`
   mutation Login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
+    login(email: $email, password: $password) {
+      token
+    }
   }
-}
 `;
 
 //tested 6/5 - working
 export const DELETE_USER = gql`
   mutation deleteUser($id: ID!) {
-  deleteUser(_id: $id) {
-    _id
+    deleteUser(_id: $id) {
+      _id
+    }
   }
-}
+`;
+
+export const ADD_ORDER = gql`
+  mutation AddOrder(
+    $services: [ID]!
+    $user: ID!
+    $provider: ID!
+    $orderPrice: Float!
+    $serviceDate: String
+  ) {
+    addOrder(
+      services: $services
+      user: $user
+      provider: $provider
+      orderPrice: $orderPrice
+      serviceDate: $serviceDate
+    ) {
+      _id
+    }
+  }
 `;
