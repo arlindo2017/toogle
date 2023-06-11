5043ccd243827878a9fd46017ab209067d3fc5ed
Arlindo Pinto <perensen@gmail.com>
2023-06-11 12:17:58 -0400

    started to add query logic to Orders and Orders.js

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,78 +1,51 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ORDERS } from "../utils/queries";
 import Order from "../components/Order";
 
 const Orders = () => {
-  const order = {
-    src: require("../images/profile/profileplaceholder.png"),
-    name: "Hart Hagerty",
-    company: "Lights On Electrical",
-    service: "Wiring Installation",
-    category: "Electrical",
-    orderDate: "05/23/2023",
-    price: "99.99",
-    serviceDate: "06/12/2023"
-  };
+  const { loading, data } = useQuery(QUERY_ORDERS);
+  const orderData = data?.orders || [];
+  console.log(orderData);
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div name="table-container" className="flex">
       <div name="row-container" className="card bg-base-100 w-full">
-         {/* header row for horizontal cards */}
-         <div name="header-row" className="flex-row justify-evenly px-8 pt-8 gap-2 hidden md:flex mx-16 mb-4">
-                <h2 className="text-[#cc451b] card-title justify-center sm:w-40">Provider Details</h2>
-                <h2 className="text-[#cc451b] card-title justify-center sm:w-48">Service Details</h2>
-                <h2 className="text-[#cc451b] card-title justify-center sm:w-44">Order Details</h2>
-          </div>
+        {/* header row for horizontal cards */}
+        <div
+          name="header-row"
+          className="flex-row justify-evenly px-8 pt-8 gap-2 hidden md:flex mx-16 mb-4"
+        >
+          <h2 className="text-[#cc451b] card-title justify-center sm:w-40">
+            Provider Details
+          </h2>
+          <h2 className="text-[#cc451b] card-title justify-center sm:w-48">
+            Service Details
+          </h2>
+          <h2 className="text-[#cc451b] card-title justify-center sm:w-44">
+            Order Details
+          </h2>
+        </div>
 
-        <Order
-          src={order.src}
-          name={order.name}
-          company={order.company}
-          service={order.service}
-          category={order.category}
-          orderDate={order.orderDate}
-          price={order.price}
-          serviceDate={order.serviceDate}
-        />
-        <Order
-          src={order.src}
-          name={order.name}
-          company={order.company}
-          service={order.service}
-          category={order.category}
-          orderDate={order.orderDate}
-          price={order.price}
-          serviceDate={order.serviceDate}
-        />
+        {orderData.map((order) => (
+          <Order
+            key={order._id}
+            // src={require(`../images/profile/${order.provider[0].profileImage}`)}
+            // name={`${order.provider.firstName} ${order.provider.lastName}`}
+            // company={order.provider.email}
+            //service={order.services[0].serviceName}
+            // category={order.services[0].serviceDesc}
+            orderDate={order.orderDate}
+            price={order.orderPrice}
+            serviceDate={order.serviceDate}
+          />
+        ))}
       </div>
-
     </div>
-    // <div className="mb-20">
-    //   {/* table start */}
-    //   <div className="overflow-x-auto w-full">
-    //     <table className="table w-full">
-    //       {/* head */}
-    //       <thead>
-    //         <tr>
-    //           {/* <th></th> */}
-    //           <th>Provider</th>
-    //           <th>Service</th>
-    //           <th>Date</th>
-    //           <th></th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         {/* row 1 */}
-    //         <Order
-    //           src={order.src}
-    //           name={order.name}
-    //           company={order.company}
-    //           service={order.service}
-    //           category={order.category}
-    //           date={order.date}
-    //         />
-    //       </tbody>
-    //     </table>
-    //   </div>
-    // </div>
   );
 };
 
