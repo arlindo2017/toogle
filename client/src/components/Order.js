96314874bff7424279b47a0aa14824a118cafe30
abmetheny <abmetheny@gmail.com>
2023-06-11 12:13:52 -0400

    Added orders page banner

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -3,7 +3,6 @@
 const Order = (props) => {
     return (
         <>
-           
             {/* row content */}
             <div name="row-cell" className="flex flex-col md:flex-row md:justify-evenly p-8 md:pt-0 gap-2 border-2 border-slate-200 rounded-2xl mx-16 mb-4">
                 {/* provider row  */}
@@ -34,34 +33,6 @@ const Order = (props) => {
                 </div>
             </div>
         </>
-
-
-
-        // <tr>
-        //     {/* <th></th> */}
-        //     <td>
-        //     <div className="flex items-center space-x-3">
-        //         <div className="avatar">
-        //         <div className="mask mask-squircle w-12 h-12">
-        //             <img src={props.src} alt="Avatar Tailwind CSS Component" />
-        //         </div>
-        //         </div>
-        //         <div>
-        //         <div className="font-bold">{props.name}</div>
-        //         <div className="text-sm opacity-50">{props.company}</div>
-        //         </div>
-        //     </div>
-        //     </td>
-        //     <td>
-        //     {props.service}
-        //     <br/>
-        //     <span className="badge badge-ghost badge-sm">{props.category}</span>
-        //     </td>
-        //     <td>{props.date}</td>
-        //     {/* <th>
-        //     <button className="btn btn-ghost btn-xs">details</button>
-        //     </th> */}
-        // </tr>
     )
 };
 
--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -13,66 +13,56 @@ const Orders = () => {
     serviceDate: "06/12/2023"
   };
   return (
-    <div name="table-container" className="flex">
-      <div name="row-container" className="card bg-base-100 w-full">
-         {/* header row for horizontal cards */}
-         <div name="header-row" className="flex-row justify-evenly px-8 pt-8 gap-2 hidden md:flex mx-16 mb-4">
-                <h2 className="text-[#cc451b] card-title justify-center sm:w-40">Provider Details</h2>
-                <h2 className="text-[#cc451b] card-title justify-center sm:w-48">Service Details</h2>
-                <h2 className="text-[#cc451b] card-title justify-center sm:w-44">Order Details</h2>
+    <div className="page-container">
+      {/* banner */}
+      <div className="hero">
+        <img
+          src={require("../images/orders-banner-2.jpeg")}
+          alt="man using power drill"
+          className="hero object-cover"
+        ></img>
+        {/* <div className="hero-overlay "></div> */}
+        <div className="text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="xs:text-2xl md:text-4xl lg:text-5xl font-bold md:mb-20 xs:mb-15">
+              You did it!
+            </h1>
           </div>
-
-        <Order
-          src={order.src}
-          name={order.name}
-          company={order.company}
-          service={order.service}
-          category={order.category}
-          orderDate={order.orderDate}
-          price={order.price}
-          serviceDate={order.serviceDate}
-        />
-        <Order
-          src={order.src}
-          name={order.name}
-          company={order.company}
-          service={order.service}
-          category={order.category}
-          orderDate={order.orderDate}
-          price={order.price}
-          serviceDate={order.serviceDate}
-        />
+        </div>
       </div>
 
+      <div name="table-container" className="flex">
+        <div name="row-container" className="card bg-base-100 w-full">
+          {/* header row for horizontal cards */}
+          <div name="header-row" className="flex-row justify-evenly px-8 pt-8 gap-2 hidden md:flex mx-16 mb-4">
+                  <h2 className="text-[#cc451b] card-title justify-center sm:w-40">Provider Details</h2>
+                  <h2 className="text-[#cc451b] card-title justify-center sm:w-48">Service Details</h2>
+                  <h2 className="text-[#cc451b] card-title justify-center sm:w-44">Order Details</h2>
+            </div>
+
+          <Order
+            src={order.src}
+            name={order.name}
+            company={order.company}
+            service={order.service}
+            category={order.category}
+            orderDate={order.orderDate}
+            price={order.price}
+            serviceDate={order.serviceDate}
+          />
+          <Order
+            src={order.src}
+            name={order.name}
+            company={order.company}
+            service={order.service}
+            category={order.category}
+            orderDate={order.orderDate}
+            price={order.price}
+            serviceDate={order.serviceDate}
+          />
+        </div>
+      </div>
     </div>
-    // <div className="mb-20">
-    //   {/* table start */}
-    //   <div className="overflow-x-auto w-full">
-    //     <table className="table w-full">
-    //       {/* head */}
-    //       <thead>
-    //         <tr>
-    //           {/* <th></th> */}
-    //           <th>Provider</th>
-    //           <th>Service</th>
-    //           <th>Date</th>
-    //           <th></th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         {/* row 1 */}
-    //         <Order
-    //           src={order.src}
-    //           name={order.name}
-    //           company={order.company}
-    //           service={order.service}
-    //           category={order.category}
-    //           date={order.date}
-    //         />
-    //       </tbody>
-    //     </table>
-    //   </div>
-    // </div>
   );
 };
 
